0b023b16401068a706dccbf5d87cbb2b0392f2ef
Dimitri <assis.dimitri@engenharia.ufjf.br>
2022-11-30 19:09:27 -0300

    feat: project ready

--- script.js
+++ script.js
@@ -1,76 +1,109 @@
-let _username = "";
-
-function signUp() {
-  const username = document.querySelector("#username").value;
-  const picture = document.querySelector("#picture").value;
-
-  axios
-    .post("http://localhost:5001/sign-up", {
-      username,
-      avatar: picture,
-    })
-    .then(() => {
-      _username = username;
-      loadTweets();
-    })
-    .catch((err) => {
-      console.error(err);
-      alert("Erro ao fazer cadastro! Consulte os logs.");
-    });
-}
-
-function loadTweets() {
-  axios.get("http://localhost:5001/tweets").then((res) => {
-    const tweets = res.data;
-    let tweetsHtml = "";
-
-    for (const tweet of tweets) {
-      tweetsHtml += `
-        <div class="tweet">
-          <div class="avatar">
-            <img src="${tweet.avatar}" />
-          </div>
-          <div class="content">
-            <div class="user">
-              @${tweet.username}
-            </div>
-            <div class="body">
-              ${escapeHtml(tweet.tweet)}
-            </div>
-          </div>
-        </div>
-      `;
-    }
-
-    document.querySelector(".tweets").innerHTML = tweetsHtml;
-    document.querySelector(".pagina-inicial").classList.add("hidden");
-    document.querySelector(".tweets-page").classList.remove("hidden");
+window.onload = () => {
+  document.querySelector(".btn-enviar").addEventListener("click", () => {
+    new Auth().signUp();
   });
+};
+
+class Auth {
+  constructor() {
+    this.username = null;
+    this.picture = null;
+  }
+
+  signUp() {
+    this.username = document.querySelector("#username").value;
+    this.picture = document.querySelector("#picture").value;
+
+    axios
+      .post("http://localhost:5001/sign-up", {
+        username: this.username,
+        avatar: this.picture,
+      })
+      .then(() => {
+        const tweet = new Tweet();
+        tweet.loadTweets();
+
+        document
+          .querySelector(".btn-enviar-tweet")
+          .addEventListener("click", () => {
+            tweet.postTweet(this.username);
+          });
+      })
+      .catch((err) => {
+        console.error(err);
+        if (err.response) {
+          alert(err.response.data);
+        }
+      });
+  }
 }
 
-function postTweet() {
-  const tweet = document.querySelector("#tweet").value;
+class Tweet {
+  constructor() {
+    this.tweet = null;
+  }
 
-  axios
-    .post("http://localhost:5001/tweets", {
-      username: _username,
-      tweet,
-    })
-    .then(() => {
-      document.querySelector("#tweet").value = "";
-      loadTweets();
-    })
-    .catch((err) => {
-      console.error(err);
-      alert("Erro ao fazer tweet! Consulte os logs.");
+  postTweet(username) {
+    this.tweet = document.querySelector("#tweet").value;
+
+    axios
+      .post("http://localhost:5001/tweets", {
+        username,
+        tweet: this.tweet,
+      })
+      .then((response) => {
+        if (response.status === 201) {
+          document.querySelector("#tweet").value = "";
+          new Tweet().loadTweets();
+          return;
+        }
+
+        console.error(response);
+        alert("Erro ao fazer tweet! Consulte os logs.");
+      })
+      .catch((err) => {
+        console.error(err);
+        if (err.response) {
+          alert(err.response.data);
+        }
+      });
+  }
+
+  loadTweets() {
+    axios.get("http://localhost:5001/tweets").then((res) => {
+      const tweets = res.data;
+      let tweetsHtml = "";
+
+      for (const tweet of tweets) {
+        tweetsHtml += `
+          <div class="tweet">
+            <div class="avatar">
+              <img src="${tweet.avatar}" />
+            </div>
+            <div class="content">
+              <div class="user">
+                @${tweet.username}
+              </div>
+              <div class="body">
+                ${this.escapeHtml(tweet.tweet)}
+              </div>
+            </div>
+          </div>
+        `;
+      }
+
+      document.querySelector(".tweets").innerHTML = tweetsHtml;
+      document.querySelector(".pagina-inicial").classList.add("hidden");
+      document.querySelector(".tweets-page").classList.remove("hidden");
     });
-}
+  }
 
-function escapeHtml(unsafe) {
-  return unsafe
-    .replace(/&/g, "&amp;")
-    .replace(/</g, "&lt;")
-    .replace(/>/g, "&gt;")
-    .replace(/"/g, "&quot;")
-    .replace(/'/g, "&#039;");
+  escapeHtml(unsafe) {
+    return unsafe
+      .replace(/&/g, "&amp;")
+      .replace(/</g, "&lt;")
+      .replace(/>/g, "&gt;")
+      .replace(/"/g, "&quot;")
+      .replace(/'/g, "&#039;");
+  }
 }
